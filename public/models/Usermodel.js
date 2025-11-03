31746481a2df5387c314073a498ca66e97f5a69a
Ezorelle <ezorelle23@gmail.com>
2025-11-03 08:11:00 +0300

    Still sorting out the frontend for seller

---
+++ public/models/Usermodel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  balance: {
+    type: Number,
+    default: 0,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
